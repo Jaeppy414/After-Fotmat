5ddbb3ba0efd4f693af8d2b2709a793b9576f274
swing0414 <swing0414@gmail.com>
2022-11-14 22:42:44 +0900

    커밋되나?

--- src/git/HelloGit.java
+++ src/git/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
+		System.out.println("git에 commit 되는 지 확인");
 	}
 
 }
--- src/git/NewFile.java
+++ src/git/NewFile.java
@@ -4,6 +4,7 @@ public class NewFile {
 
 	public static void main(String[] args) {
 		System.out.println("New File");
+		System.out.println("git에 commit 되는 지 확인");
 	}
 
 }
